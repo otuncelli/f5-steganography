73d8a9605cdf42b04f597d8dae8cb3565a8561f7
username1565 <39200328+username1565@users.noreply.github.com>
2019-05-30 17:13:15 +0200

    Add packages, and batch files to compile and test.
    Add two nupkg packages, from F5Lib.csproj
    
    BouncyCastle.Crypto
    "..\packages\BouncyCastle.1.7.0\lib\Net40-Client\BouncyCastle.Crypto.dll"
    
    log4net
    "..\packages\log4net.2.0.3\lib\net40-full\log4net.dll"
    
    See "/packages/Readme.txt"
    Add: compile.bat and "/F5Console/bin/Release/test_f5_compiled.bat"
    Add two test files: "/F5Console/bin/Release/cat.jpg" and "/F5Console/bin/Release/test.txt"
    _____________________________________________________________________________
    How to compile?
    
    Run compile.bat, and wait.
    See files:
    "\F5Console\bin\Release\cat_contains_test.jpg"
    and
    "F5Console\bin\Release\extracted_test.txt"
    after test.

---
+++ F5Console/bin/Release/test_f5_compiled.bat
@@ -0,0 +1,2 @@
+F5.exe e -e test.txt -p mypasswd -q 70 cat.jpg cat_contains_test.jpg
+F5.exe x -p mypasswd -e extracted_test.txt cat_contains_test.jpg
---
+++ compile.bat
@@ -0,0 +1,3 @@
+C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe /property:Configuration=Release F5Console\F5Console.csproj
+cd F5Console\bin\Release
+test_f5_compiled.bat
